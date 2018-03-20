5994210afda8fc8f1b07ec20c6f8601045e59c10
guttas <guttas@163.com>
2018-03-20 21:30:57 +0800

    Finish upload api

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,6 @@ var util = require('util');
 
 
 var router = express.Router();
-var logger = log4js.getLogger();
-logger.level = 'debug';
 
 
 router.use(function timeLog(req, res, next) {
@@ -14,49 +12,29 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 
-router.get('/', function(req, res) {
-  res.send('Birds home page');
-});
-
-router.put('/', function(req, res) {
-	console.log('req: ', req.body);
-
-    res.send('Update the book');
-});
-
 router.get('/about', function(req, res) {
-  res.send('About birds');
+  res.send('This is the 1.0 API of AR Viewer');
 });
 
 router.post('/upload', function(req, res) {
-	var form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files) {
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.end(util.inspect({fields: fields, files: files}));
-    });
-	
-	
-/*     form.uploadDir = "../tmp";
+  var form = new formidable.IncomingForm();
+    form.uploadDir = "./tmp";
     form.keepExtensions = true;
     form.multiples = false; // we are not ready on multiple support
     form.parse(req, function(err, fields, files) {
-		console.log(util.inspect({err:err, fields1: fields, files1: files })); */
+		console.log(util.inspect({fields1: fields, files1: files }));
 		
-/*     	var uploadFile = files.file;
+    var uploadFile = files.file;
 
-		if (uploadFile.name == undefined) {
+		if (uploadFile == undefined) {
 			res.send(err);
 			console.log("upload failed.");
-			logger.info('upload failed: invalid file name.');
 			return;
 		}
 
-		logger.info('upload request: ' + uploadFile.name);
-		console.log("upload success: "+uploadFile.path);  */
-
-	//	res.send('upload success');
-    //});
+		console.log("upload success: "+uploadFile.path);
+		res.send(uploadFile.path.substr(4));
+    });
 });
 
 module.exports = router;
