f40be01dc13dd06a189c93b3f4ccf77b6dd1b007
ADS\guota <guttas@163.com>
2018-03-23 17:25:51 +0800

    Commit the workflow to use promise

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,7 @@
 var express = require('express');
-var formidable = require('formidable')
 var log4js = require('log4js');
 var util = require('util');
-
+var formidable = require('formidable')
 
 var router = express.Router();
 
@@ -16,25 +15,69 @@ router.get('/about', function(req, res) {
   res.send('This is the 1.0 API of AR Viewer');
 });
 
+function upload2forge(filePath)
+{
+  console.log('upload2forge');
+}
+
+function translate2dae(filePath)
+{
+  console.log('translate2dae');
+}
+
 router.post('/upload', function(req, res) {
-  var form = new formidable.IncomingForm();
+   
+  var parser = new Promise(function(resolve, reject) {
+
+    var form = new formidable.IncomingForm();
     form.uploadDir = "./tmp";
     form.keepExtensions = true;
     form.multiples = false; // we are not ready on multiple support
     form.parse(req, function(err, fields, files) {
-		console.log(util.inspect({fields1: fields, files1: files }));
-		
+    console.log(util.inspect({fields1: fields, files1: files }));
+
     var uploadFile = files.file;
+    if (uploadFile != undefined) {
 
-		if (uploadFile == undefined) {
-			res.send(err);
-			console.log("upload failed.");
-			return;
-		}
+      console.log("formidable upload success.");
+      return resolve(uploadFile);
+    }
 
-		console.log("upload success: "+uploadFile.path);
-		res.send(uploadFile.path.substr(4));
+    console.log("formidable upload failed.");
+    return reject('error');
     });
+  });
+
+  parser.then(function(uploadFile) {
+
+    var upload = filePath => new Promise((resolve, reject) => {
+      console.log('filePath is : ' + filePath);
+      upload2forge(filePath);
+
+      return resolve(filePath.substr(4));
+    });
+
+    var translate = filePath => new Promise((resolve, reject) => {
+      console.log('filePath is : ' + filePath);
+      translate2dae(filePath);
+
+      return resolve(filePath.substr(4));
+    });
+
+    Promise.all([upload(uploadFile.path), translate(uploadFile.path)]).then(v =>{
+      res.send(v[0]);
+      return;
+    }).catch(function(err){
+      console.log(err);
+      res.send('error');
+      return;
+    });
+
+  }).catch(function(err){
+    console.log(err);
+    res.send('error');
+    return;
+  }); 
 });
 
 module.exports = router;
